0cda97cb5e11d7fbaf0670e43206c39746307217
Julia Bandurska <julka.ban@gmail.com>
2024-05-23 19:23:40 +0200

    When user is loading button 'new' is disabled

--- src/app/people/pages/people-page/people-page.component.ts
+++ src/app/people/pages/people-page/people-page.component.ts
@@ -1,35 +1,52 @@
-import { AsyncPipe } from '@angular/common';
 import { Component, OnDestroy, OnInit, inject } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { Timer } from '../../../shared/classes/timer';
 import { FullNamePipe } from '../../../shared/pipes/full-name.pipe';
+import { User } from '../../models/user.model';
 import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-people-page',
   standalone: true,
-  imports: [AsyncPipe, FullNamePipe],
+  imports: [FullNamePipe],
   templateUrl: './people-page.component.html',
   styleUrl: './people-page.component.scss',
 })
 export class PeoplePageComponent implements OnInit, OnDestroy {
   private userService = inject(UserService);
-  protected user$ = this.userService.user$;
+  protected user: User | null = null;
+  protected isLoading = true;
 
   private timer!: Timer;
 
+  private userSub!: Subscription;
+
   ngOnInit(): void {
+    this.userSub = this.userService.user$.subscribe({
+      next: (user) => {
+        this.user = user;
+        this.isLoading = false;
+      },
+    });
     this.userService.getNewUser();
+
     this.timer = new Timer(() => {
-      this.userService.getNewUser();
+      this.getNewUser();
     }, 5000);
   }
 
   ngOnDestroy(): void {
+    this.userSub.unsubscribe();
     this.timer.destroy();
   }
 
   protected getNewUser(): void {
     this.userService.getNewUser();
+    this.isLoading = true;
+  }
+
+  protected getNewUserAndReset(): void {
+    this.getNewUser();
     this.timer.reset();
   }
 

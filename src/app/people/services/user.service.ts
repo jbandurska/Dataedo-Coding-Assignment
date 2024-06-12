1178080968fa39a3a1d62fc0e5cb30690e2b392b
Julia Bandurska <julka.ban@gmail.com>
2024-06-12 18:05:22 +0200

    Request canceled when next one is being sent

--- src/app/people/services/user.service.ts
+++ src/app/people/services/user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, map, tap } from 'rxjs';
+import { BehaviorSubject, Subscription, map, tap } from 'rxjs';
 import { RandomUserAPIService } from '../../feature/services/random-user-api.service';
 import { User } from '../models/user.model';
 
@@ -10,10 +10,16 @@ export class UserService {
   private userSubject = new BehaviorSubject<User | null>(null);
   public user$ = this.userSubject.asObservable();
 
+  private getRandomUserSub?: Subscription;
+
   constructor(private randomUserAPIService: RandomUserAPIService) {}
 
   public getNewUser(): void {
-    this.randomUserAPIService
+    if (this.getRandomUserSub) {
+      this.getRandomUserSub.unsubscribe();
+    }
+
+    this.getRandomUserSub = this.randomUserAPIService
       .getRandomUser()
       .pipe(
         map((response) => response.results[0]),
